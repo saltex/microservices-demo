79c5875450509e0c6d6285d264f1090890e6acbc
Akelsandr Saltykov <oleksandr.saltykov@globant.com>
2018-09-21 07:01:54 +0300

    currency-conversion-server

--- currency-exchange-service/src/main/java/com/example/currencyexchangeservice/CurrencyExchangeController.java
+++ currency-exchange-service/src/main/java/com/example/currencyexchangeservice/CurrencyExchangeController.java
@@ -3,6 +3,7 @@ package com.example.currencyexchangeservice;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.math.BigDecimal;
@@ -14,7 +15,7 @@ public class CurrencyExchangeController {
     private Environment environment;
 
     @GetMapping("currency-exchange/from/{from}/to/{to}")
-    public ExchangeValue retrieveExchangeValue() {
+    public ExchangeValue retrieveExchangeValue(@PathVariable String from, @PathVariable String to) {
         ExchangeValue value = new ExchangeValue(100L, "UAH", "USD", BigDecimal.valueOf(27));
         value.setPort(environment.getProperty("local.server.port"));
         return value;
