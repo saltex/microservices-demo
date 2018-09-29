b86726c1d701e7d43e619d437977a714154f0e64
Akelsandr Saltykov <oleksandr.saltykov@globant.com>
2018-09-29 16:04:00 +0300

    currency-conversion-server

--- currency-conversion-service/src/main/java/com/example/currencyconversionservice/CurrencyConversionService.java
+++ currency-conversion-service/src/main/java/com/example/currencyconversionservice/CurrencyConversionService.java
@@ -1,5 +1,7 @@
 package com.example.currencyconversionservice;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -10,12 +12,15 @@ import java.math.BigDecimal;
 @RestController
 public class CurrencyConversionService {
 
+    private Logger logger = LoggerFactory.getLogger(this.getClass());
+
     @Autowired
     private CurrencyExchangeProxy currencyExchangeProxy;
 
     @GetMapping("/currency-converter-feign/from/{from}/to/{to}/quantity/{quantity}")
     public CurrencyConvertionBean conversionServiceFiegn(@PathVariable String from, @PathVariable String to, @PathVariable BigDecimal quantity) {
         CurrencyConvertionBean response = currencyExchangeProxy.retrieveExchangeValue(from, to);
+        logger.info("{}", response);
         return new CurrencyConvertionBean(from,to,response.getQuantity(), response.getConversionMultiple(),
                 quantity.multiply(response.getConversionMultiple()), response.getPort());
     }
--- currency-exchange-service/src/main/java/com/example/currencyexchangeservice/CurrencyExchangeController.java
+++ currency-exchange-service/src/main/java/com/example/currencyexchangeservice/CurrencyExchangeController.java
@@ -1,5 +1,7 @@
 package com.example.currencyexchangeservice;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,6 +13,9 @@ import java.math.BigDecimal;
 @RestController
 public class CurrencyExchangeController {
 
+    private Logger logger = LoggerFactory.getLogger(this.getClass());
+
+
     @Autowired
     private Environment environment;
 
@@ -18,6 +23,7 @@ public class CurrencyExchangeController {
     public ExchangeValue retrieveExchangeValue(@PathVariable String from, @PathVariable String to) {
         ExchangeValue value = new ExchangeValue(100L, "UAH", "USD", BigDecimal.valueOf(27));
         value.setPort(environment.getProperty("local.server.port"));
+        logger.info("{}", value);
         return value;
     }
 }
